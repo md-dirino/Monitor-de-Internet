04613dd373acd186cd24d168657b805cf77881e7
md-dirino <maycon9748@hotmail.com>
2025-02-15 07:51:28 -0300

    tentando empacotar

--- forge.config.js
+++ forge.config.js
@@ -4,24 +4,21 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 module.exports = {
   packagerConfig: {
     asar: true,
+    icon: "icon", // Agora aponta diretamente para "icon.ico" na raiz do projeto
   },
   rebuildConfig: {},
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
-      config: {},
+      config: {
+        name: "Monitor_de_Internet",
+        setupIcon: "icon.ico", // Ícone do instalador na raiz do projeto
+        noMsi: true, // Mantém apenas o .exe
+      },
     },
     {
       name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
-    },
-    {
-      name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
+      platforms: ['win32'],
     },
   ],
   plugins: [
@@ -29,8 +26,6 @@ module.exports = {
       name: '@electron-forge/plugin-auto-unpack-natives',
       config: {},
     },
-    // Fuses are used to enable/disable various Electron functionality
-    // at package time, before code signing the application
     new FusesPlugin({
       version: FuseVersion.V1,
       [FuseV1Options.RunAsNode]: false,
