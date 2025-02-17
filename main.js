56014b91f38330bb6eda08f2feb37b8f4fcdf65b
md-dirino <maycon9748@hotmail.com>
2025-02-17 09:45:42 -0300

    Ajustes na janela de exibição

--- main.js
+++ main.js
@@ -1,22 +1,40 @@
-const { app, BrowserWindow } = require('electron');
-const { autoUpdater } = require("electron-updater");
+const { app, BrowserWindow, nativeTheme } = require('electron'); // Importa as bibliotecas necessárias
 
 let mainWindow;
 
+// Cria a janela
+const createWindow = () => {
+  nativeTheme.themeSource = 'dark'; // Define o tema como dark pra janela
+  mainWindow = new BrowserWindow ({
+      width: 800,
+      height: 600,
+      //resizable: false, // Desabilita o redimensionamento da janela
+      webPreferences: {
+          nodeIntegration: true
+      },
+      icon: "resources/app/icon.ico", // Define o ícone da janela
+      autoHideMenuBar: true, // Oculta a barra de menu
+      //titleBarStyle: 'hidden' // Oculta a barra de título e menu
+  })
+
+  // Carrega o arquivo HTML local
+  mainWindow.loadFile("index.html");                                            
+};
+
+// Cria a janela quando o aplicativo é iniciado
 app.whenReady().then(() => {
-    mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true
-        },
-        icon: "resources/app/icon.ico" // Define o ícone da janela
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
     });
+});
 
-    // Remove o menu padrão
-    const { Menu } = require('electron');
-    Menu.setApplicationMenu(null);
-
-    // Carrega o arquivo HTML local
-    mainWindow.loadFile("index.html");
+// Fecha a janela quando o aplicativo é fechado
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
