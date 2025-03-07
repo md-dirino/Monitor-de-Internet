8f5e36a720bee15afd8dab336c0c32cd6f355be3
md-dirino <maycon9748@hotmail.com>
2025-03-07 14:48:01 -0300

    Alterar formato da mensagem de commit para nova versão gerada

--- release-flow.js
+++ release-flow.js
@@ -49,7 +49,7 @@ async function main() {
 
     // Commit curto para indicar nova versão
     execSync("git add package.json", { stdio: "inherit" });
-    execSync(`git commit -m "🚀 Nova versão gerada ${newVersion}"`, { stdio: "inherit" });
+    execSync(`git commit -m "Nova versão ${newVersion} gerada"`, { stdio: "inherit" });
 
     // Criar e enviar a tag
     execSync(`git tag v${newVersion}`, { stdio: "inherit" });
